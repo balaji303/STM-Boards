178bc03adc1750708c39abb2dd38bd2b93b326b4
Balaji <balajivelu303@outlook.com>
2020-06-20 22:20:27 +0530

    Update GeneralPurposeTimerForOutput.c

--- Nucleo/STM32/STM23F446RE/Timer/GeneralPurposeTimerForOutput.c
+++ Nucleo/STM32/STM23F446RE/Timer/GeneralPurposeTimerForOutput.c
@@ -1,1 +1,22 @@
+/*
+Led Blink using General Purpose Timer Output Capture in TIM2
+*/
+#include "stm32f4xx.h"                  // Device header
+
+int main(void){
+	RCC->AHB1ENR |=0x1;
+	GPIOA->MODER |=0x800;
+	GPIOA->AFR[0]|=0x100000;
+	
+	RCC->APB1ENR |=0X1;
+	TIM2->PSC  =1600-1;
+	TIM2->ARR  =10000-1;
+	TIM2->CCMR1 =0x30;
+	TIM2->CCR1 = 0;
+	TIM2->CCER |=1; 
+	TIM2->CNT  = 0;
+	TIM2->CR1  = 1;
+	
+	while(1){}
+}
 
