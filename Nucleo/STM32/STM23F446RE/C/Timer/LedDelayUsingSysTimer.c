54a884f80b348586d3f5086af0ff8914aa9d0c4f
balaji303 <balajivelu303@outlook.com>
2021-04-23 08:12:50 +0530

    FriM

--- Nucleo/STM32/STM23F446RE/C/Timer/LedDelayUsingSysTimer.c
+++ Nucleo/STM32/STM23F446RE/C/Timer/LedDelayUsingSysTimer.c
@@ -12,9 +12,9 @@ int main(void){
 	GPIOA->MODER |=0X400;                 //Enable PA5 output mode
 	
 	//SysTick timer
-	SysTick->LOAD = 6400000-1;            //Changing  load value changes delay
-	SysTick->VAL = 0;
-	SysTick->CTRL = 5;
+	SysTick->LOAD = 6400000-1;            /*Changing  load value changes delay. Mmo. of cycles in single ms, if XTAL=64MHz then 64M cycles in one sec which is 64M/1000 in one millisec. Load takes values for 1ms*/
+	SysTick->VAL = 0; /*Clear the current value register*/
+	SysTick->CTRL = 5; /*Enable it*/
 	
 	while(1){
 		if(SysTick->CTRL & 0x10000){
